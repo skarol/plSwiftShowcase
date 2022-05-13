996fa4a77c56fec577b7301dea7c1299e74688c1
Karolina Samorek <karolina.samorek@gmail.com>
2022-05-13 09:01:58 +0200

    Add Desing System Tokens

---
+++ DesignSystem/Sources/DesignSystem/Tokens/CornerRadiuses.swift
@@ -0,0 +1,22 @@
+//
+//  CornerRadiuses.swift
+//  
+//
+//  Created by Karolina Samorek on 12/05/2022.
+//
+
+import Foundation
+import CoreGraphics
+import DesignSystemUtils
+
+public extension DSTokens.CornerRadiuses {
+    /// The Small Corner Radius size.
+    /// Equal to ``6``.
+    static let s: CGFloat = 6
+    /// The Medium Corner Radius size.
+    /// Equal to ``16``.
+    static let m: CGFloat = 16
+    /// The Large Corner Radius size.
+    /// Equal to ``24``.
+    static let l: CGFloat = 24
+}
---
+++ DesignSystem/Sources/DesignSystem/Tokens/Spaces.swift
@@ -0,0 +1,31 @@
+//
+//  Spaces.swift
+//  
+//
+//  Created by Karolina Samorek on 12/05/2022.
+//
+
+import Foundation
+import CoreGraphics
+import DesignSystemUtils
+
+public extension DSTokens.Spaces {
+    /// The Extra Small Space size.
+    /// Equal to ``4``.
+    static let xs: CGFloat = 4
+    /// The Small Space size.
+    /// Equal to ``8``.
+    static let s: CGFloat = 8
+    /// The Medium Space size.
+    /// Equal to ``12``.
+    static let m: CGFloat = 12
+    /// The Large Space size.
+    /// Equal to ``24``.
+    static let l: CGFloat = 24
+    /// The Extra Large Space size.
+    /// Equal to ``48``.
+    static let xl: CGFloat = 48
+    /// The Double Extra Large Space size.
+    /// Equal to ``64``.
+    static let xxl: CGFloat = 64
+}
