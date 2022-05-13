1f5bde940a58e3d35e5d8d2c2e519d62270bcd52
Karolina Samorek <karolina.samorek@gmail.com>
2022-05-13 09:01:16 +0200

    Add DesignSystem local package

---
+++ DesignSystem/Sources/DesignSystem/Styles/DSStyles.swift
@@ -0,0 +1,17 @@
+//
+//  DSStyles.swift
+//
+//
+//  Created by Karolina Samorek on 12/05/2022.
+//
+
+import Foundation
+
+public enum DSStyles {
+    /// All DesignSystem Test Styles namespace.
+    public enum Texts {}
+    /// All DesignSystem Button Styles namespace.
+    public enum Buttons {}
+    /// All DesignSystem Shadows Styles namespace.
+    public enum Shadows {}
+}
---
+++ DesignSystem/Sources/DesignSystem/Tokens/DSTokens.swift
@@ -0,0 +1,20 @@
+//
+//  DSTokens.swift
+//  
+//
+//  Created by Karolina Samorek on 12/05/2022.
+//
+
+import Foundation
+
+/// All DesignSystem Tokens namespace.
+public enum DSTokens {
+    /// All DesignSystem Colors namespace.
+    public enum Colors {}
+    /// All DesignSystem Fonts namespace.
+    public enum Fonts {}
+    /// All DesignSystem Spaces namespace.
+    public enum Spaces {}
+    /// All DesignSystem Corner Radiuses namespace.
+    public enum CornerRadiuses {}
+}
